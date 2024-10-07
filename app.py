25c6c3108da7f26112b6447116d318cfdda88a78
Karely3412 <karely@devpipeline.com>
2024-10-07 07:10:44 -0600

    imported models to app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import psycopg2
-import os
 from flask import Flask
 from flask_marshmallow import Marshmallow
+import os
 from db import *
+
+from models.imessages import Imessages
+from models.users import Users
 
 
 app = Flask(__name__)
@@ -10,3 +13,5 @@ app = Flask(__name__)
 
 if __name__ ==  '__main__':
     app.run(host= '0.0.0.0', port='8086')
+
+# left of on 7.2 curr
