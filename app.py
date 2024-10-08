f799d60531506322046eadf814c6f8583665823a
Karely3412 <karely@devpipeline.com>
2024-10-07 18:10:50 -0600

    added create_all() func into app.py

--- app.py
+++ app.py
@@ -13,9 +13,17 @@ app = Flask(__name__)
 app.congfig['DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config["SQLALCHEY_TRACK_MODIFICATIONS"] = False
 
+init_db(app, db)
+
+def create_tables():
+    with app.app_context():
+        print("Creating Tables...")
+        db.create_all()
+        print("Tables created successfully")
+
+create_tables()
 
 
 if __name__ ==  '__main__':
     app.run(host= '0.0.0.0', port='8086')
 
-# left of on 7.2 curr
--- models/imessages.py
+++ models/imessages.py
@@ -1,2 +1,1 @@
-class Imessages:
-    
+class Imessages(db.):
